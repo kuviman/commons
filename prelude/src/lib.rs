4a541cbe8384fef9214edaae2d4ad0db84e579af
kuviman <kuviman@gmail.com>
2018-01-21 21:46:41 +0400

    thread_rng & random are simple reexports

--- prelude/src/lib.rs
+++ prelude/src/lib.rs
@@ -43,7 +43,7 @@ pub use num::{clamp, Float, Integer, Num};
 pub use owning_ref::{OwningHandle, OwningRef, OwningRefMut};
 
 #[doc(no_inline)]
-pub use rand::Rng;
+pub use rand::{random, thread_rng, Rng};
 
 mod color;
 mod algebra;
@@ -58,34 +58,6 @@ pub use range::*;
 pub use timer::*;
 pub use acell::*;
 pub use stable_fn::*;
-
-pub fn thread_rng() -> Box<Rng> {
-    #[cfg(target_os = "emscripten")]
-    {
-        extern "C" {
-            fn emscripten_random() -> c_float;
-        }
-        struct EmscriptenRng;
-        impl Rng for EmscriptenRng {
-            fn next_u32(&mut self) -> u32 {
-                unsafe { (emscripten_random() as f64 * std::u32::MAX as f64) as u32 }
-            }
-            fn next_f32(&mut self) -> f32 {
-                unsafe { emscripten_random() as f32 }
-            }
-            fn next_f64(&mut self) -> f64 {
-                unsafe { emscripten_random() as f64 }
-            }
-        }
-        Box::new(EmscriptenRng)
-    }
-    #[cfg(not(target_os = "emscripten"))]
-    Box::new(rand::thread_rng())
-}
-
-pub fn random<R: rand::Rand>() -> R {
-    R::rand(&mut thread_rng())
-}
 
 pub fn default<T: Default>() -> T {
     T::default()
