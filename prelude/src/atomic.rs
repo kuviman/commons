749fa56adac60443b18ea87f82024ed75d04a1fd
kuviman <kuviman@gmail.com>
2018-01-21 20:23:05 +0400

    Simplified AtomicCell (no emscripten-specific code)

--- prelude/src/atomic.rs
+++ prelude/src/atomic.rs
@@ -1,29 +1,15 @@
 use ::*;
 
-#[derive(Debug)]
 pub struct AtomicCell<T: Copy> {
-    #[cfg(target_os = "emscripten")]
-    inner: Cell<T>,
-    #[cfg(not(target_os = "emscripten"))]
     inner: Mutex<T>,
 }
 
-#[cfg(target_os = "emscripten")]
-impl<T: Copy> AtomicCell<T> {
-    pub fn new(value: T) -> Self {
-        Self {
-            inner: Cell::new(value),
-        }
-    }
-    pub fn get(&self) -> T {
-        self.inner.get()
-    }
-    pub fn set(&self, value: T) {
-        self.inner.set(value);
+impl<T: Copy + Debug> Debug for AtomicCell<T> {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(fmt, "{:?}", self.get())
     }
 }
 
-#[cfg(not(target_os = "emscripten"))]
 impl<T: Copy> AtomicCell<T> {
     pub fn new(value: T) -> Self {
         Self {
