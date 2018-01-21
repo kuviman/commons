5367d79e13a97f08a6da42b0470f54a4e5fe349c
kuviman <kuviman@gmail.com>
2018-01-21 22:08:28 +0400

    One-letter Color fields

--- prelude/src/color.rs
+++ prelude/src/color.rs
@@ -1,20 +1,20 @@
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
 pub struct Color {
-    pub red: f32,
-    pub green: f32,
-    pub blue: f32,
-    pub alpha: f32,
+    pub r: f32,
+    pub g: f32,
+    pub b: f32,
+    pub a: f32,
 }
 
 macro_rules! rgb {
     ($r:expr, $g:expr, $b:expr) => { rgb!($r, $g, $b, 1.0) };
     ($r:expr, $g:expr, $b:expr, $a: expr) => {
         Color {
-            red: $r,
-            green: $g,
-            blue: $b,
-            alpha: $a,
+            r: $r,
+            g: $g,
+            b: $b,
+            a: $a,
         }
     };
 }
@@ -31,29 +31,19 @@ impl Color {
     pub const MAGENTA: Color = rgb!(1.0, 0.0, 1.0);
     pub const CYAN: Color = rgb!(0.0, 1.0, 1.0);
 
-    pub fn rgb(red: f32, green: f32, blue: f32) -> Self {
-        Color {
-            red,
-            green,
-            blue,
-            alpha: 1.0,
-        }
+    pub fn rgb(r: f32, g: f32, b: f32) -> Self {
+        Color { r, g, b, a: 1.0 }
     }
-    pub fn rgba(red: f32, green: f32, blue: f32, alpha: f32) -> Self {
-        Color {
-            red,
-            green,
-            blue,
-            alpha,
-        }
+    pub fn rgba(r: f32, g: f32, b: f32, a: f32) -> Self {
+        Color { r, g, b, a }
     }
 
     pub fn argb_hex(value: u32) -> Self {
         Color {
-            red: ((value >> 16) & 0xff) as f32 / 255.0,
-            green: ((value >> 8) & 0xff) as f32 / 255.0,
-            blue: (value & 0xff) as f32 / 255.0,
-            alpha: (value >> 24) as f32 / 255.0,
+            r: ((value >> 16) & 0xff) as f32 / 255.0,
+            g: ((value >> 8) & 0xff) as f32 / 255.0,
+            b: (value & 0xff) as f32 / 255.0,
+            a: (value >> 24) as f32 / 255.0,
         }
     }
 }
