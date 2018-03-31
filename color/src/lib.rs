c37f1685343aef365036c301222c8bec20c93ab3
kuviman <kuviman@gmail.com>
2018-03-31 19:49:28 +0400

    Color::distance_to

--- color/src/lib.rs
+++ color/src/lib.rs
@@ -102,4 +102,12 @@ impl Color {
         }
         Color { r, g, b, a }
     }
+
+    pub fn distance_to(self, other: Color) -> f32 {
+        let dr = self.r - other.r;
+        let dg = self.g - other.g;
+        let db = self.b - other.b;
+        let da = self.a - other.a;
+        (dr * dr + dg * dg + db * db + da * da).sqrt()
+    }
 }
