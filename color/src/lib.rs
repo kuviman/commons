2eb8e5d5efa5408f692f8556eadb8fcbe925adc0
kuviman <kuviman@gmail.com>
2018-03-31 19:29:05 +0400

    Color::rgb_hex

--- color/src/lib.rs
+++ color/src/lib.rs
@@ -44,12 +44,21 @@ impl Color {
         Color { r, g, b, a }
     }
 
-    pub fn argb_hex(value: u32) -> Self {
+    pub fn rgb_hex(hex: u32) -> Self {
         Color {
-            r: ((value >> 16) & 0xff) as f32 / 255.0,
-            g: ((value >> 8) & 0xff) as f32 / 255.0,
-            b: (value & 0xff) as f32 / 255.0,
-            a: (value >> 24) as f32 / 255.0,
+            r: ((hex >> 16) & 0xff) as f32 / 255.0,
+            g: ((hex >> 8) & 0xff) as f32 / 255.0,
+            b: (hex & 0xff) as f32 / 255.0,
+            a: 1.0,
+        }
+    }
+
+    pub fn argb_hex(hex: u32) -> Self {
+        Color {
+            r: ((hex >> 16) & 0xff) as f32 / 255.0,
+            g: ((hex >> 8) & 0xff) as f32 / 255.0,
+            b: (hex & 0xff) as f32 / 255.0,
+            a: (hex >> 24) as f32 / 255.0,
         }
     }
 }
