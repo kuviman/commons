fffa8a3e85129eb1745558bc2fa5aa873ea1ebac
kuviman <kuviman@gmail.com>
2018-04-01 14:46:19 +0400

    Small changes in docs

--- timer/src/lib.rs
+++ timer/src/lib.rs
@@ -1,7 +1,10 @@
+//! Timer.
+
 #![deny(warnings)]
 
 extern crate prelude;
 
+/// Timer can be used to track time since some instant.
 pub struct Timer {
     start: std::time::Instant,
 }
@@ -11,19 +14,19 @@ fn to_secs(duration: std::time::Duration) -> f64 {
 }
 
 impl Timer {
-    /// Constructs a new timer
+    /// Constructs a new timer.
     pub fn new() -> Self {
         Self {
             start: std::time::Instant::now(),
         }
     }
 
-    /// Get time elapsed (in seconds) since last reset
+    /// Get time elapsed (in seconds) since last reset.
     pub fn elapsed(&self) -> f64 {
         to_secs(self.start.elapsed())
     }
 
-    /// Reset, and get time elapsed (in seconds) since last reset
+    /// Reset, and get time elapsed (in seconds) since last reset.
     pub fn tick(&mut self) -> f64 {
         let now = std::time::Instant::now();
         let delta = now.duration_since(self.start);
