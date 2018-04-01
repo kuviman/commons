d8dee48c805eed491ee2b787796e289963997246
kuviman <kuviman@gmail.com>
2018-04-01 14:47:47 +0400

    More docs for rng crate

--- rng/src/lib.rs
+++ rng/src/lib.rs
@@ -1,3 +1,5 @@
+//! Random number generation.
+
 #![deny(warnings)]
 
 extern crate prelude;
@@ -8,6 +10,7 @@ pub use rand::{thread_rng, Rng, distributions::{Distribution, Uniform}};
 
 pub(crate) use prelude::*;
 
+/// Generate a random value.
 pub fn gen<T>() -> T
 where
     Uniform: Distribution<T>,
@@ -15,19 +18,21 @@ where
     thread_rng().gen()
 }
 
+/// Generate a random value from a given range.
 pub fn gen_range<T: PartialOrd + rand::distributions::range::SampleRange>(range: Range<T>) -> T {
     thread_rng().gen_range(range.start, range.end)
 }
 
+/// Randomly shuffle an array.
 pub fn shuffle<T>(values: &mut [T]) {
     thread_rng().shuffle(values);
 }
 
-/// Chooses random element
+/// Choose a random element from an array.
 ///
 /// # Panics
 ///
-/// Panics if values is an empty slice
+/// Panics if values is an empty slice.
 pub fn choose<T>(values: &[T]) -> &T {
     thread_rng()
         .choose(values)
